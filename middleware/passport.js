ac7e5f5306ac40fe9a6e820b2e0f116a4d147d97
Aidan Christopher <gandamovies@gmail.com>
2021-03-21 15:23:53 -0700

    fixed the login issue

--- middleware/passport.js
+++ middleware/passport.js
@@ -3,7 +3,7 @@ const LocalStrategy = require("passport-local").Strategy;
 const GitHubStrategy = require('passport-github').Strategy;
 const TwitchStrategy = require("passport-twitch-new").Strategy;
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const userController = require("../controller/auth_controller");
+const userController = require("../controller/auth_controller").authController;
 const userModel = require("../models/userModel").userModel;
 const localLogin = new LocalStrategy(
   {
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -60,7 +60,7 @@ router.get('/google/callback',
   passport.authenticate('google', { failureRedirect: '/auth/login' }),
   function(req, res) {
     // Successful authentication, redirect home.
-    res.redirect('/');
+    res.redirect('/reminders');
   });
 
 module.exports = router;
