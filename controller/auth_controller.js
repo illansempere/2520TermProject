e6a085e205107c9bd7e81202969ef09471a8e374
Aidan Christopher <gandamovies@gmail.com>
2021-03-21 14:21:02 -0700

    for illan

--- controller/auth_controller.js
+++ controller/auth_controller.js
@@ -1,6 +1,27 @@
 let database = require("../database");
 let userModel = require("../models/userModel").userModel;
 
+const getUserByEmailIdAndPassword = (email, password) => {
+  let user = userModel.findOne(email);
+  if (user) {
+    if (isUserValid(user, password)) {
+      return user;
+    }
+  }
+  return null;
+};
+const getUserById = (id) => {
+  let user = userModel.findById(id);
+  if (user) {
+    return user;
+  }
+  return null;
+};
+
+function isUserValid(user, password) {
+  return user.password === password;
+}
+
 let authController = {
   login: (req, res) => {
     res.render("/auth/login");
@@ -57,5 +78,8 @@ let authController = {
 };
 
 
-
-module.exports = authController;
+module.exports = {
+  getUserByEmailIdAndPassword,
+  getUserById,
+  authController,
+};
