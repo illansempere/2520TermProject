2a8b6bd2fa6cf6bfb4aece9daa73a27829fee26f
Aidan Christopher <gandamovies@gmail.com>
2021-04-13 22:05:07 -0700

    added the address section to the reminder

--- controller/reminder_controller.js
+++ controller/reminder_controller.js
@@ -5,6 +5,7 @@ const passport = require("../middleware/passport");
 let userdatabase = require("../models/userModel").database;
 let userModel = require("../models/userModel").userModel;
 const request = require('request-promise');
+// const {Client} = require("@googlemaps/google-maps-services-js");
 
 async function getWeather(){
   var x;
@@ -13,6 +14,21 @@ async function getWeather(){
     })
     return x
 }
+
+// client
+//   .elevation({
+//     params: {
+//       locations: [{ lat: 45, lng: -110 }],
+//       key: "asdf",
+//     },
+//     timeout: 1000, // milliseconds
+//   })
+//   .then((r) => {
+//     console.log(r.data.results[0].elevation);
+//   })
+//   .catch((e) => {
+//     console.log(e.response.data.error_message);
+//   });
 
 let remindersController = {
   list: (req, res) => {
@@ -71,6 +87,12 @@ let remindersController = {
     if (exists == false) {
       AddUser(currentuser)
     }
+    let tempdb = {
+      address: req.body.address,
+      city: req.body.city,
+      stateprovince: req.body.stateprovince,
+      country: req.body.country
+    }
 
     let reminder = {
       id: database[currentuser].reminders.length + 1,
@@ -79,8 +101,11 @@ let remindersController = {
       description: req.body.description,
       subtasks: req.body.subtasks,
       tags: req.body.tags,
+      locationbool: req.body.locationTF,
+      location: tempdb,
       completed: false,
     };
+    console.log(reminder)
     database[currentuser].reminders.push(reminder);
     res.redirect("/reminders");
   },
@@ -115,7 +140,12 @@ let remindersController = {
     if (exists == false) {
       AddUser(currentuser)
     }
-
+    let tempdb = {
+      address: req.body.address,
+      city: req.body.city,
+      stateprovince: req.body.stateprovince,
+      country: req.body.country
+    }
     let reminderToFind = req.params.id;
     let updatedreminder = {
       id: reminderToFind,
@@ -124,14 +154,15 @@ let remindersController = {
       description: req.body.description,
       subtasks: req.body.subtasks,
       tags: req.body.tags,
+      locationbool: req.body.locationTF,
+      location: tempdb,
       completed: req.body.completed,
     };
     let searchResult = database[currentuser].reminders.find(function (reminder) {
       return reminder.id == reminderToFind;
     });
-    console.log(searchResult)
     let del = database[currentuser].reminders.indexOf(searchResult)
-    database[currentuser].reminders.splice(del);
+    database[currentuser].reminders.splice(del,1);
     database[currentuser].reminders.push(updatedreminder);
     res.render("reminder", { reminderItem: searchResult, reminders: database[currentuser].reminders });
   },
